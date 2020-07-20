b0ef6c8efcbf896234b0737d22989cffa11900c2
ATSpitzer <ATSpitzer@users.noreply.github.com>
2020-07-20 11:46:49 -0700

    Fixed test to start vpn servers when PageExplorer object is created

--- tests/vpn_test.py
+++ tests/vpn_test.py
@@ -1,30 +1,32 @@
 import unittest
 from Page_Explorer.LeakTest.LeakTest_Explorer import LeakTestExplorer
 from Vpn_Tool.VpnClient import VpnClient
+import os
 
 class MyTestCase(unittest.TestCase):
     COUNTRY_CONV = {
         "usa": "United States",
-        # "uk": "United Kingdom",
+        "uk": "United Kingdom",
         "india": "India"
     }
-    # vpn_dictionary = {}
-    # for country in COUNTRY_CONV:
-    #     vpn_dictionary[country] = {}
-    #     vpn_dictionary[country]['vpn'] = VpnClient(country)
-    #     vpn_dictionary[country]['vpn'].start_vpn()
-    #     vpn_dictionary[country]['webdriver'] = LeakTestExplorer(country=country)
+    config_dir = os.path.join('/','etc','shadowsocks-libev')
+    vpn_configs = VpnClient.load_configs(config_dir)
 
     def check_country(self, test_country_short):
-        test_ip = self.vpn_dictionary[test_country_short]['vpn'].config_options[test_country_short]['server']
+        """
+        Starts vpn client connection to server at specified county. Then opens dnsleaktest.com and checks:
+         - That the ip-address displayed is the address of the vpn server
+         - That the country (corresponding to the address) shown is the country specified in the client config
+        :param test_country_short: country
+        """
+        test_ip = self.vpn_configs[test_country_short]['server']
         try:
             test_country_long = self.COUNTRY_CONV[test_country_short]
         except:
             print("Country tested not found in abbreviation dictionary")
             raise
 
-        # le = self.vpn_dictionary[test_country_short]['webdriver']
-        le = LeakTestExplorer(test_country_short)
+        le = LeakTestExplorer(country=test_country_short)
         le.describe_connection()
 
         found_ip=le.ip_address
